22dbfa0625eeb32cbb6e328c437682d383bed791
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2022-10-25 22:55:13 +0300

    Add files via upload

---
+++ ros_workspace1/build/glass_bridge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/michaelsk/workspace_ros1/src/glass_bridge
+# Build directory: /home/michaelsk/workspace_ros1/build/glass_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros_workspace1/build/glass_bridge/catkin_generated/installspace/glass_bridge-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(glass_bridge_MESSAGE_FILES "")
+set(glass_bridge_SERVICE_FILES "srv/glass_service.srv")

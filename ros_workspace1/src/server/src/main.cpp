0d4a3c85e72007d6d91a894794f202206722a205
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2022-10-25 22:44:17 +0300

    Add files via upload

---
+++ ros_workspace1/src/server/src/main.cpp
@@ -0,0 +1,12 @@
+#include "glass_bridge.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "glass_bridge_server");
+  ros::init(argc, argv, "publisher");
+  ros::init(argc, argv, "subscriber");
+  GlassBridge first;
+  //std::cout << first; //to observe initial string
+  ros::spin();
+  return 0;
+}

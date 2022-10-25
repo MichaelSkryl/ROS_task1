253ff8a1fb200b31fe02b54cbb5f30e611c5287e
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2022-10-25 22:51:51 +0300

    Add files via upload

---
+++ ros_workspace1/build/server/CMakeFiles/server_trial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/michaelsk/workspace_ros1/devel/lib/server/server_trial"
+  "/home/michaelsk/workspace_ros1/devel/lib/server/server_trial.pdb"
+  "CMakeFiles/server_trial.dir/src/main.cpp.o"
+  "CMakeFiles/server_trial.dir/src/publisher_subscriber.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server_trial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_workspace1/build/server/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/michaelsk/workspace_ros1/src/server
+# Build directory: /home/michaelsk/workspace_ros1/build/server
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

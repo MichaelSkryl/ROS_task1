9ff32cc0797c595a47a80bf0de787be78cdbc4f5
MichaelSkryl <90468326+MichaelSkryl@users.noreply.github.com>
2022-10-25 22:56:27 +0300

    Add files via upload

---
+++ ros_workspace1/build/glass_bridge/CMakeFiles/glass_bridge_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/michaelsk/workspace_ros1/devel/include/glass_bridge/glass_service.h"
+  "CMakeFiles/glass_bridge_generate_messages_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/glass_bridge_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_workspace1/build/glass_bridge/CMakeFiles/glass_bridge_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/michaelsk/workspace_ros1/devel/lib/python3/dist-packages/glass_bridge/srv/__init__.py"
+  "/home/michaelsk/workspace_ros1/devel/lib/python3/dist-packages/glass_bridge/srv/_glass_service.py"
+  "CMakeFiles/glass_bridge_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/glass_bridge_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
